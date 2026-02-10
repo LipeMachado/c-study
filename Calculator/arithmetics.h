1480479511a868497c8d055df9c4f7d28f678a43
LipeMachado <silvalipe765@gmail.com>
2026-02-09 22:53:30 -0300

    I learned a little about Headers and Makefiles.

---
+++ Calculator/arithmetics.h
@@ -0,0 +1,7 @@
+#ifndef _ARITHMETICS_H_
+#define _ARITHMETICS_H_
+
+int sum(int a, int b);
+int minus(int a, int b);
+
+#endif
---
+++ Calculator/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "arithmetics.h"
+
+int main(){
+	printf("Hello World!\n");
+	
+	printf("%d\n", sum(1, 1));
+	printf("%d\n", minus(5, 2));
+
+	return 0;
+}
