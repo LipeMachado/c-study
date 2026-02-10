5dab854fe7dd5780df25f53e6e00fec7ebe56bf6
LipeMachado <silvalipe765@gmail.com>
2026-02-10 12:41:25 -0300

    Add chooses

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "arithmetics.h"
+
+void cleanScreen(){
+	#ifdef _WIN32
+        	system("cls"); // Windows
+	#else
+		system("clear"); // Linux/Unix
+	#endif
+}
 
 int main(){
 	printf("Hello World!\n");
+
+	int choose;
+	int num1;
+	int num2;
 	
-	printf("%d\n", sum(1, 1));
-	printf("%d\n", minus(5, 2));
+	while(1){
+		printf("\n1 - Sum \n2 - Minus \n0 - Exit\n");
+		printf("Choose a option:\n");
+		scanf("%d", &choose);
+		
+		switch(choose){
+			case 1:
+				printf("First number: \n");
+				scanf("%d", &num1);
+				printf("Secound number: \n");
+				scanf("%d", &num2);
+				cleanScreen();
+				printf("Result: %d \n", sum(num1, num2));
+				break;
+			case 0:
+				cleanScreen();
+				printf("Saída do programa!\n");
+				return 0;
+			default:
+				cleanScreen();
+				printf("Valor incorreto!");
+		}
+	}
 
 	return 0;
 }
